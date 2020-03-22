693ef5cd8e13228d32cf46b7ff33cb5bca787c16
junru-zhu <61392832+junru-zhu@users.noreply.github.com>
2020-03-22 00:53:45 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	// print the text "Hello world!"
+	printf("Hello world!\n");
+	// print a random number whose value is between 0-99
+	printf("%d\n", rand()%100);
+	// print a blank line
+	printf("\n");
+
+	return 0;
+}
